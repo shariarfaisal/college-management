8e1aeb20d616c910d75f6eec912c094b74ffa3fb
shariarfaisal <faisaljr623@gmail.com>
2020-01-06 17:57:23 +0600

    backend url changed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import { STUDENT, TEACHER, ADMIN } from './utils/naming'
 
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+const httpLink = new HttpLink({ uri: process.env.FCI_COLLEGE_FONTEND  || 'http://localhost:4000' });
 
 const authLink = new ApolloLink((operation, forward) => {
   const adminToken = localStorage.getItem(ADMIN);
