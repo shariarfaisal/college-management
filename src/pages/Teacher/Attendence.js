f193e6a4e854e9e375025b0e23b077c2b2443676
shariarfaisal <faisaljr623@gmail.com>
2020-11-07 10:22:33 +0600

    break point fixed

--- src/pages/Teacher/Attendence.js
+++ src/pages/Teacher/Attendence.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TeacherLayout from '../../layouts/TeacherLayout'
-import Body from '../../components/teacherProfile/attendence/Index'
+import Body from '../../components/teacherProfile/attendence/index'
 
 const Attendence = ({match}) => {
   return (
